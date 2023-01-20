f23ecb2897778e9e75950446e858c9738cdfcb6e
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-20 16:23:06 -0600

    Add funcntion to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const split = string.split('');
+    let output = '';
+    for(i = split.length - 1; i >= 0; i--){
+        output += split[i];
+    }
 
+    return output;
 };
 
 // Do not edit below this line
