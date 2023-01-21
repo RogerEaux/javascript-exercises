9e162203ff2c91ad8a7be5f0490b05726896430c
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-20 19:39:59 -0600

    Add functions to convert from F° to C° and viceversa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp;
+  convertedTemp = Math.round((temp-32)*5/9*10)/10;
+  
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convertedTemp;
+  convertedTemp = Math.round((temp*9/5 + 32)*10)/10;
+  
+  return convertedTemp;
 };
 
-// Do not edit below this line
+// Do not edit below this lineT
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
