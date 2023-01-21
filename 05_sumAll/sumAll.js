40e06ec24b8c8ce86cc14306701deef981b1510e
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-20 18:31:00 -0600

    Add function to get the sum of all natural numbers bewtween two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum;
+    const difference = Math.abs(first - second) + 1;
+    if(!(Number.isInteger(first) && Number.isInteger(second) && first > 0 && second > 0)){
+        return 'ERROR';
+    }
+    sum = difference/2*(first+second);
 
+    return sum;
 };
 
 // Do not edit below this line
